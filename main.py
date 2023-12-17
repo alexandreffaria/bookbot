a26f81dce864d174a159fdbc7fd72284efd10c5d
Alexandre F <alexandrefonsecaffaria@gmail.com>
2023-12-16 23:27:48 -0300

    countWords() and countLetters working full report

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("yoyo")
+def countWords(text):
+    wordsList = text.split()
+    return len(wordsList)
+
+def countLetters(text):
+    countingDict = {}
+    for letter in text:
+        lowered = letter.lower()
+        if lowered not in countingDict:
+            countingDict[lowered] = 1
+        else:
+            countingDict[lowered] += 1
+    return countingDict
+
+frankenstein = "books/frankenstein.txt"
+
+with open(frankenstein) as f:
+    text = f.read()
+    
+    print(f"\n| Reporting on {frankenstein} |\n")
+    print(f"| {countWords(text)} words were found in the document.\n")
+
+    letters = countLetters(text)
+    list_letters = list(letters)
+    list_letters.sort()
+    
+    for letter in list_letters:
+        if letter.isalpha():
+            print(f"| The '{letter}' character was found {letters[letter]} times.")
+
+    print("\n| End of the report ~ Hope you are happy! ❤ |")
